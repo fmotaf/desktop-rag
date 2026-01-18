56f5cc6e9cb1719e3b830cd77f0d795457d2520e
Fernando Mota <nando.mot@gmail.com>
2026-01-17 22:00:54 -0300

    some modifications and testing, still clueless LOL

---
+++ ui/postcss.config.js
@@ -0,0 +1,6 @@
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {},
+    autoprefixer: {}, // already in your package.json
+  },
+};
--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,7 +1,22 @@
 import './style.css'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <p> Hello World! </p>
-  </div>
-`
+function returnPage(){
+  const app = document.querySelector<HTMLDivElement>('#app');
+  const p = document.createElement('p');
+
+
+  p.textContent = "aaaaaaaasdasdasa";
+
+  app?.append(p);
+
+  return app;
+}
+
+
+export default returnPage();
+
+// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+//   <div>
+//     <p> Hello World! </p>
+//   </div>
+// `
